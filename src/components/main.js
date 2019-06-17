34802a8b4057c3c4aa8eaf15321889c7e9e6b632
Robin <swesson92@gmail.com>
2019-06-17 16:46:32 +0200

    setup

---
+++ src/components/main.js
@@ -0,0 +1,19 @@
+//Den klassen är för Routing. Vi importerar inte Component eftersom detta inte är en component class.
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import LandingPage from './landingpage';
+import Aboutme from './aboutme';
+import Contact from './contact';
+import Projects from './projects';
+import Resume from './resume';
+
+const Main = () => (
+    <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/aboutme" component={Aboutme} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/projects" component={Projects}/>
+        <Route path="/resume" component={Resume}/>
+    </Switch>
+)
+export default Main;
