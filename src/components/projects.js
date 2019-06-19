8522e94fcf0972b7186e6c0fb86ca990185fdf21
Robin <swesson92@gmail.com>
2019-06-19 19:26:58 +0200

    project page updated again

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, CardText, Button, CardMenu, IconButton} from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, CardText, CardMenu, IconButton} from 'react-mdl';
 
 
 class Projects extends Component{
@@ -14,30 +14,32 @@ class Projects extends Component{
                 return(
             <div className="projects-grid">
 <Card shadow={0} style={{width: '512px', margin: 'auto'}}>
-    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png) center / cover'}}>My React Project</CardTitle>
+    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png) center / cover'}}>TicTacToe</CardTitle>
     <CardText>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Mauris sagittis pellentesque lacus eleifend lacinia...
+     A project to learn to code in React framework. Simple TicTacToe game with some fun functions.
     </CardText>
     <CardActions border>
-    <Button colored>Github</Button>
-    <Button colored>Live Demo</Button>
-    <Button colored>Share</Button>
+    <div className="gittis">
+        <a href="https://github.com/Swesson92/LearningReact" rel="noopener noreferrer"target="_blank">
+            <i className="fa fa-github-square"  aria-hidden="true"/>
+        </a>
+    </div>
     </CardActions>
     <CardMenu style={{color: '#fff'}}>
     <IconButton name="share" />
     </CardMenu>
 </Card>
 <Card shadow={0} style={{width: '512px', margin: 'auto'}}>
-    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png) center / cover'}}>My React Project</CardTitle>
+    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn-images-1.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png) center / cover'}}>Website about me</CardTitle>
     <CardText>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Mauris sagittis pellentesque lacus eleifend lacinia...
+        Building a website in React to learn further about the framework. The idea with this project is to continue implement functions and different things to do on the site.
     </CardText>
     <CardActions border>
-    <Button colored>Github</Button>
-    <Button colored>Live Demo</Button>
-    <Button colored>Share</Button>
+    <div className="gittis">
+        <a href="https://github.com/Swesson92/mySite" rel="noopener noreferrer"target="_blank">
+            <i className="fa fa-github-square"  aria-hidden="true"/>
+        </a>
+    </div>
     </CardActions>
     <CardMenu style={{color: '#fff'}}>
     <IconButton name="share" />
@@ -51,17 +53,16 @@ class Projects extends Component{
             return(
                 <div className="projects-angular">
                 <Card shadow={0} style={{width: '512px', margin: 'auto'}}>
-                    <CardTitle style={{color: 'black', height: '176px', background: 'url(http://www.zelearner.com/wp-content/uploads/2015/02/angular-825x510.png) center / cover'}}>CRG Client</CardTitle>
+                    <CardTitle style={{color: 'black', height: '210px', background: 'url(http://www.zelearner.com/wp-content/uploads/2015/02/angular-825x510.png) center / cover'}}>CRG Client</CardTitle>
                     <CardText>
-                    Five unemployed dudes with one given goal, one destination, 
-                    to create a community for devoted video-game admirers to share their their thoughts and opinions. 
-                    This quest meant a long distance from society and human interraction, but since none of them were familiar with it anyway they took their equipment and settled in a deep dark dungeon in the dank forest of Scandanavia. 
-                    ....legend says that they are stil in that dungeon
+                    Client for CRG project. Built with Angular 7 and connected to a backend written in Java. This client contains things like Localstorage, Sessionstorage, Errorhandling and more, along with REST calls to backend for information/rating about games saved in the database.
                     </CardText>
                     <CardActions border>
-                    <Button colored>Github</Button>
-                    <Button colored>Live Demo</Button>
-                    <Button colored>Share</Button>
+            <div className="gittis">
+                    <a href="https://github.com/ArastoSahbaei/CommunityRatesGames/tree/development/client" rel="noopener noreferrer"target="_blank">
+                        <i className="fa fa-github-square"  aria-hidden="true"/>
+                    </a>
+                </div>
                     </CardActions>
                     <CardMenu style={{color: '#fff'}}>
                     <IconButton name="share" />
@@ -82,7 +83,11 @@ class Projects extends Component{
                     ....legend says that they are stil in that dungeon
                     </CardText>
                     <CardActions border>
-                    <Button colored>Github</Button>
+                    <div className="gittis">
+                    <a href="https://github.com/ArastoSahbaei/CommunityRatesGames" rel="noopener noreferrer"target="_blank">
+                        <i className="fa fa-github-square"  aria-hidden="true"/>
+                    </a>
+                </div>
                     </CardActions>
                     <CardMenu style={{color: '#fff'}}>
                     <IconButton name="share" />
