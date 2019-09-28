92562bb98c23c9f6ae403617d6d22da278dec904
Robin <swesson92@gmail.com>
2019-09-28 15:32:18 +0200

    added logic for weather rest

--- src/components/weather/weatherservice.js
+++ src/components/weather/weatherservice.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Forms from './forms';
+import Titles from './titles';
+import Weather from './weather';
 
 const API_KEY = "ba0c17210935d83717a61c2b8509e83b";
 
@@ -13,7 +16,63 @@ class Weatherservice extends Component {
     }
     getWeather = async (e) => {
         e.preventDefault();
+        const city = e.target.element.city.value;
+        const country = e.target.element.country.value;
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+        const data = api_call.json();
+
+        //To check after data
+        if(city && country){
+            this.setState({
+                temperature: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                humidity: data.sys.humidity,
+                description: data.weather[0].description,
+                error: ""
+            });
+
+        } else {
+            this.setState({
+                temperature: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                description: undefined,
+                error: "Please enter values"
+            });
+        }
     }
+
+    render() {
+        return (
+          <div>
+            <div className="wrapper">
+              <div className="main">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-xs-5 title-container">
+                      <Titles />
+                    </div>
+                    <div className="col-xs-7 form-container">
+                      <Form getWeather={this.getWeather} />
+                      <Weather 
+                        temperature={this.state.temperature} 
+                        humidity={this.state.humidity}
+                        city={this.state.city}
+                        country={this.state.country}
+                        description={this.state.description}
+                        error={this.state.error}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      }
 }
 
 export default Weatherservice;
