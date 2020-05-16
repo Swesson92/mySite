920c0833a3df8883fee3e5335d8b0df1173ebe86
Robin <robin.svensson06@gmail.com>
2020-05-16 12:20:34 +0200

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Contact from './Contact';
+import Content from './Content';
+import { Route, Link, Switch, BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+
+
+const routing = (
+    <Router>
+      <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/content">Content</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/content" component={Content} />
+        <Route path="/contact" component={Contact} />
+    </Switch>
+      </div>
+    </Router>
+  )
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
