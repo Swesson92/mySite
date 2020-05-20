006bb4a91c6c8c3aacf23126b4b0acad22e30ee6
Robin <robin.svensson06@gmail.com>
2020-05-20 14:34:13 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './App.css'
 
-import Toolbar from './components/Toolbar/Toolbar';
-import SideDrawer from './components/SideDrawer/SideDrawer';
-import Backdrop from './components/Backdrop/Backdrop';
-import Content from './components/Toolbar/Content';
-import Contact from './components/Toolbar/Contact';
-import { Route } from "react-router-dom";
 
-class App extends Component {
-  state = {
-    sideDrawerOpen: false
-  };
+class App extends Component{
 
-  drawerToggleClickHandler = () => {
-    this.setState((prevState) => {
-      return {sideDrawerOpen: !prevState.sideDrawerOpen};
-    });
-  };
-
-  backdropClickHandler = () => {
-    this.setState({sideDrawerOpen: false});
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Robin Svensson',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' }
+      ],
+      home: {
+        title: 'Title Text',
+        subTitle: 'This is subtitle',
+        text: 'Just text'
+      },
+      about: {
+        title: 'About Me'
+      },
+      contact: {
+        title: 'Let\'s Talk'
+      }
+    }
+  }
 
   render() {
-    let backdrop;
-
-    if (this.state.sideDrawerOpen) {
-      backdrop = <Backdrop click={this.backdropClickHandler} />
-    }
     return (
-      <div style={{height: '100%'}}>
-        <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
-        <SideDrawer show={this.state.sideDrawerOpen} />
-        {backdrop}
-        <main style={{marginTop: '64px'}}>
-        </main>
-      <Route path="/content" component={Content} />
-      <Route path="/contact" component={Contact} />  
-      </div>
-    );
-  }
-}
+     <Router>
 
+     </Router>
+      );
+    }
+  }
 export default App;
