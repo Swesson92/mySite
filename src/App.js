22d60e8e80609c59bcc6e3ed6d7ffd1f38f8276d
Robin <robin.svensson06@gmail.com>
2020-05-26 18:22:17 +0200

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css'
 
 
@@ -32,8 +34,20 @@ class App extends Component{
   render() {
     return (
      <Router>
-       <Container>
-         <p>Hejsan</p>
+       <Container className="p-0" fluid={true}>
+       <Navbar className="border-bottom" bg="transparent" expand="lg">
+        <Navbar.Brand>
+          Robin Svensson
+        </Navbar.Brand>
+        <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+        </Navbar>
        </Container>
      </Router>
       );
