f3b2b9de4d8af01c3d84f67588e38f4bba1fda65
Robin <robin.svensson06@gmail.com>
2020-05-27 11:02:04 +0200

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import react from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function Footer(){
+
+    return(
+        <footer className="mt-5">
+            <Container>
+                <Row>
+                    <Col>
+                    
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+}
+
+
+
+export default Footer;
