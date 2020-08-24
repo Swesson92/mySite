564d913c74023cb49ba00cd43a2af8630175a9ea
Robin <robin.svensson06@gmail.com>
2020-08-24 20:36:04 +0200

    updates

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
 
-function ContactPage(props){
+class ContactPage extends React.Component(props){
 
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            message: '',
+            disabled: '',
+            emailSent: '',
+
+        }
+
+    }
+
+
+    render(){
     return (
-
+        
         <p>ContactPage works!</p>
     );
+
+    }
 }
 
 
