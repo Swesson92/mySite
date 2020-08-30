08e5d2d8512701b8b561c0f1a54988ac2f9ec341
Robin <robin.svensson06@gmail.com>
2020-08-30 17:27:45 +0200

    small changes in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,12 +11,11 @@ function Footer(){
     return(
         <footer className="mt-5">
             <Container fluid={true}>
-                <Row className="border-top justify-content-between p-3">
+                <Row className="border-top  p-3">
                     <Col className="p-0" md={3} sm={12}>
                     <Socialmedia />
                     </Col>
                     <Col className="p-0 d-flex justify-content-end" md={3}>
-                        robin.svensson06@gmail.com
                     </Col>
                 </Row>
             </Container>
