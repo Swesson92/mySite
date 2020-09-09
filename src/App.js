bd6b7975e95e743ab192f4dfcdd8bcef4940260a
Robin <robin.svensson06@gmail.com>
2020-09-09 18:01:17 +0200

    added some more backend functionality

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component{
       ],
       home: {
         title: 'Web Development.',
-        subTitle: 'Aerial photo',
+        subTitle: 'Photography',
         text: 'Feel free to contact me'
       },
       about: {
