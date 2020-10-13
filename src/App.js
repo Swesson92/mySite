744076991d52d4bd9f9fa1843bdf85a46cbf01cf
Robin <robin.svensson06@gmail.com>
2020-10-14 01:54:28 +0200

    reduced size images and new color

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component{
       ],
       home: {
         title: 'Web Development.',
-        subTitle: 'Photography',
+        subTitle: 'Photography/Video',
         text: 'Feel free to contact me'
       },
       about: {
