10fbbb91bd1a821d40222f37bd497a08a48a3f2d
Robin <robin.svensson06@gmail.com>
2020-10-23 14:05:06 +0200

    added navbar, button etc

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,37 @@
-import React, {useState} from 'react';
+import React, {useState , useEffect} from 'react';
 import {Link} from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
 
 function Navbar() {
 
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
-
     const closeMobileMenu = () => (false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+      }, []);
+
+
+    window.addEventListener('resize', showButton);
+    
     return (
         <>
         <nav className="navbar">
             <div className="navbar-container">
         
-                <Link to="/" className="navbar-logo">
+                <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                     Robin <i className='fab fa-typo3' />
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
@@ -36,6 +54,7 @@ function Navbar() {
                         </Link>
                     </li>
                 </ul>
+                {button && <Button buttonStyle='btn--outline'>TEST</Button>}
             </div>
         </nav>
         </>
