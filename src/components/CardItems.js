f739e24e0263ffbfe7f09c40852257a264cbcf7c
Robin <robin.svensson06@gmail.com>
2020-10-28 14:52:27 +0100

    testing

--- src/components/CardItems.js
+++ src/components/CardItems.js
@@ -9,7 +9,7 @@ function CardItems(props) {
             <figure className='cards__item__pic-wrap' data-category={props.label}>
               <img
                 className='cards__item__img'
-                alt='Travel Image'
+                alt='Travel'
                 src={props.src}
               />
             </figure>
