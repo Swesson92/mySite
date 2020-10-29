17f8f11e4b326e7432a5c34eafc839ffb725d84e
Robin <robin.svensson06@gmail.com>
2020-10-29 14:20:38 +0100

    started with footer

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <section className='footer-subscription'>
+                <p className='footer-subscription-heading'>
+                    Join here!
+                </p>
+                <p className='footer-subscription-text'>
+                    You can unsubscribe
+                </p>
+            </section>
+            
+        </div>
+    )
+}
+
+export default Footer
